fd6fe690853488172a35520f3da9d88c8e489bb0
dundin10190 <101626231+dundin10190@users.noreply.github.com>
2022-03-15 15:55:35 +0800

    Update test.r

--- test.r
+++ test.r
@@ -1,10 +1,14 @@
+```{r}
+ShannonWiener <- function(filePath) {
+  twforest <- fread(filePath)
+  # 計算總物種數
+  S <- twforest[, sum(individuals) ]
+  # 計算個別物種數佔總物種數之比例
+  p_i <- twforest[, individuals/S ]
+  # 最後加總並算出 Shannon-Wiener (H') 多樣性指數
+  H <- -sum(p_i * log(p_i))
+  return(H)
+}
 
-### 生態資訊學範例: function
-
-helloWorld <- function(username= "小熊維尼"){
-   # sprintf('Hello! %s', username)
-    print(paste("Hello! ", username, sep = ))
-  }
-#'Hello! %s' %s為變數
-helloWorld('小熊')
-
+ShannonWiener("twforest.csv")
+```
